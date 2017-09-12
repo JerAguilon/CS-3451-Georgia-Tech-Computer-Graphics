f29f546a5fc113ab023e12cfe080d55266f18a35
Jeremy <jeraguilon@gmail.com>
2017-09-12 13:57:34 -0600

    Progress

--- HW2/drawing_test/drawlib.py
+++ HW2/drawing_test/drawlib.py
@@ -3,6 +3,7 @@
 from matlib import *
 
 activeMatrix = None
+vertices = []
 
 def gtOrtho(left, right, bottom, top, near, far):
     print("FOOBAR")
@@ -30,12 +31,25 @@ def gtPerspective(fov, near, far):
 def gtBeginShape():
     pass
 
+
 def gtEndShape():
     pass
 
 def gtVertex(x, y, z):
+    global vertices
+    if len(vertices) == 0:
+        vertices.append(multiplyVector(stack.peek(), [x,y,z,1]))
+        return
     topMatrix = stack.peek()
-    x, y, z = multiply((x,y,z), topMatrix)
+    lastVector = multiplyVector(stack.peek(), vertices[0])
+    currVector = multiplyVector(stack.peek(), [x,y,z,1])
+    line(lastVector[0], currVector[0], lastVector[1], currVector[1])
+    vertices = []
     
-def multiply(vector, matrix):
-    pass
+def multiplyVector(matrix, vector):
+    output = []
+    print(matrix)
+    print(vector)
+    for i in range(4):
+        output.append(matrix[i][0]*vector[0] + matrix[i][1]*vector[1] + matrix[i][2]*vector[2] + matrix[i][3]*vector[3])
+    return output
